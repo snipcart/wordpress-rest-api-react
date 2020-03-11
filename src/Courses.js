1ed9b9e48f8cfe24f8d858a981e46a52d0d77580
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2020-03-11 11:39:00 -0400

    add missing component

---
+++ src/Courses.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import useFetch from './useFetch';
+
+export default function Courses() {
+    const courses = useFetch('http://localhost/wp-json/wp/v2/courses');
+  return (
+    <List component="nav" aria-label="main mailbox folders">
+        {courses && courses.map((course, index) => (
+        <ListItem key={index}>
+          <ListItemText primary={course.title.rendered} />
+        </ListItem>
+        ))}
+      </List>
+  );
+}
