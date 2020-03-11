1f625bf3af8fb967df6c697985a56ebb9f596765
Jean-Sébastien Tremblay <jeansebtr@gmail.com>
2020-03-11 17:27:33 -0400

    add signout

--- src/App.js
+++ src/App.js
@@ -4,23 +4,44 @@ import Typography from '@material-ui/core/Typography';
 
 import Courses from './Courses';
 import Posts from './Posts';
+import { useSnipcartState } from './snipcart';
 
 function App() {
+  const isSignedIn = useSnipcartState(state => state.customer.status === 'SignedIn');
+
+  function signOut(ev) {
+    ev.preventDefault();
+    window.Snipcart.api.customer.signout();
+  }
+
   return (
     <Container maxWidth="sm">
       <header>
         <Typography variant="h2">
           Online courses for the API enthusiast
         </Typography>
+        {isSignedIn ? (
+          <p>
+            <button onClick={signOut}>
+              Signout
+            </button>
+          </p>
+        ) : (
+          <p>
+            <button className="snipcart-customer-signin">
+              Signin to purchase our courses
+            </button>
+          </p>
+        )}
       </header>
       <section>
-        <Typography variant="h3">
+        <Typography variant="h4">
           Courses from top teachers
         </Typography>
         <Courses />
       </section>
       <section>
-        <Typography variant="h3">
+        <Typography variant="h4">
           From our blog
         </Typography>
         <Posts />
--- src/Courses.js
+++ src/Courses.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import SchoolIcon from '@material-ui/icons/School';
+
+import { useSnipcartState } from './snipcart';
 import useFetch from './useFetch';
 
+
 export default function Courses() {
-    const courses = useFetch('/wp-json/wp/v2/courses');
+    const courses = useFetch('http://localhost/wp-json/wp/v2/courses');
+
+    const isSignedIn = useSnipcartState(state => state.customer.status === 'SignedIn');
   return (
     <List component="nav" aria-label="main mailbox folders">
         {courses && courses.map((course, index) => (
         <ListItem key={index}>
-          <ListItemText primary={course.title.rendered} />
+          <ListItemIcon>
+            <SchoolIcon />
+          </ListItemIcon>
+          <ListItemText primary={course.title.rendered} secondary={isSignedIn? `Buy for ${course.price}$` : null} />
+          {isSignedIn && (<ListItemSecondaryAction
+            className="snipcart-add-item"
+            data-item-id={course.id}
+            data-item-name={course.title.rendered}
+            data-item-price={course.price}
+            data-item-url="https://snipcart-wordpress-react.herokuapp.com/wp-json/wp/v2/courses">
+            <IconButton edge="end" aria-label="delete">
+              <AddShoppingCartIcon />
+            </IconButton>
+          </ListItemSecondaryAction>)}
         </ListItem>
         ))}
       </List>
